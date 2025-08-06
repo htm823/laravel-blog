f480473db26565c715fbce7a444421a8896a157d
htm823 <choco.cacao31@gmail.com>
2025-08-07 06:50:35 +0900

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>My Blog</title>
+</head>
+<body>
+	<h1>My Blog</h1>
+	<ul>
+		<li>Post</li>
+		<li>Post</li>
+		<li>Post</li>
+		<li>Post</li>
+	</ul>
+</body>
+</html>
